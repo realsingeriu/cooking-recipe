96b432b74a4d1f59bceafae44a985010a74b6ae6
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-24 14:54:52 +0900

    navbar 오류 수정 및 ThemeContext파일 jsx로 변경

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useContext } from "react"
 import Searchbar from "./Searchbar";
 import { Link } from "react-router-dom";
+import './Navbar.css';
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
+  const {color} = useContext(ThemeContext)
 return (
+    <div className="navbar" style={{backgroundColor: color}}>
       <nav>
         <Link to="/" className='brand'>
           <h1>쿠킹 레시피</h1>
@@ -11,6 +15,7 @@ return (
         <Searchbar />
         <Link to="/create">레시피 만들기</Link>
       </nav>
+      </div>
   );
 }
       
