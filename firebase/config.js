758cf371f2b392bdedac98cb4fd27021f24e3998
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-27 12:46:53 +0900

    API secret 설정 (.env 생성)

--- firebase/config.js
+++ firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   //본인의 파이어베이스 구성객체를 복사해 넣는다.
-  apiKey: "AIzaSyCCb3ayDrylSdw2aWME488UKsNHjRDaG3k",
-  authDomain: "cooking-recipe-b57a1.firebaseapp.com",
-  projectId: "cooking-recipe-b57a1",
-  storageBucket: "cooking-recipe-b57a1.appspot.com",
-  messagingSenderId: "985814654675",
-  appId: "1:985814654675:web:7c632cd02fc7aa87657e26",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 //init firebase
