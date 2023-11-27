13fc1ff5beb5fbdef85b342142bd6de9c8f9a7b9
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-27 10:08:29 +0900

    오류로 인해 수정

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
 import './RecipeList.css';
-import { useContext } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
+
 
 
 export default function RecipeList({ recipes, mode }) {
-  const {mode} = useContext(ThemeContext)
+  
   if (recipes.length === 0) {
     return <div className='error'>검색된 레시피가 없습니다.</div>;
   }
